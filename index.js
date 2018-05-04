fbca0258a6a37c1a2394e29193f8370d3f116238
flybyme <mangoraft@gmail.com>
2018-05-03 20:10:03 -0500

    update dns to es6/await

--- index.js
+++ index.js
@@ -29,15 +29,85 @@ const methods = [
     'router.remove.url',
     'router.remove.host',
     'router.remove.tls',
+    /*
+     *Fleet methods
+     */
+    'fleet.container.stop',
+    'fleet.container.start',
+    'fleet.allocate.resources',
+    'fleet.app.deploy',
+    'fleet.app.scale',
+    'fleet.container.start',
+    'fleet.container.stop',
+    'fleet.container.restart',
+    'fleet.container.move',
+    'fleet.deploy',
+    'fleet.scale',
+    'fleet.node.get',
 ]
 
 methods.forEach(function (method) {
-    module.exports[method] = function (data) {
+
+    let methods = method.split('.')
+
+    function fn(data) {
         return new Promise(function (resolve, reject) {
             var job = jobs.create(method, data);
             job.on('complete', resolve);
             job.on('failed', reject);
-            job.save(reject);
+            job.save(function (err) {
+                if (err) {
+                    reject(err)
+                }
+            });
         })
+    }
+
+    switch (methods.length) {
+        case 1:
+            if (!module.exports[methods[0]]) {
+                module.exports[methods[0]] = {}
+            }
+            module.exports[methods[0]] = fn
+            break;
+        case 2:
+            if (!module.exports[methods[0]]) {
+                module.exports[methods[0]] = {}
+            }
+            if (!module.exports[methods[0]][methods[1]]) {
+                module.exports[methods[0]][methods[1]] = {}
+            }
+
+            module.exports[methods[0]][methods[1]] = fn
+            break;
+        case 3:
+            if (!module.exports[methods[0]]) {
+                module.exports[methods[0]] = {}
+            }
+            if (!module.exports[methods[0]][methods[1]]) {
+                module.exports[methods[0]][methods[1]] = {}
+            }
+            if (!module.exports[methods[0]][methods[1]][methods[2]]) {
+                module.exports[methods[0]][methods[1]][methods[2]] = {}
+            }
+            module.exports[methods[0]][methods[1]][methods[2]] = fn
+            break;
+        case 4:
+            if (!module.exports[methods[0]]) {
+                module.exports[methods[0]] = {}
+            }
+            if (!module.exports[methods[0]][methods[1]]) {
+                module.exports[methods[0]][methods[1]] = {}
+            }
+            if (!module.exports[methods[0]][methods[1]][methods[2]]) {
+                module.exports[methods[0]][methods[1]][methods[2]] = {}
+            }
+            if (!module.exports[methods[0]][methods[1]][methods[2]][methods[3]]) {
+                module.exports[methods[0]][methods[1]][methods[2]][methods[3]] = {}
+            }
+            module.exports[methods[0]][methods[1]][methods[2]][methods[3]] = fn
+            break;
+        default:
+            module.exports[methods[0]] = fn
     }
 })
